3a7e01cda6dd52cbf49947f863088d98726c7a0d
Nicolás Soto <105766645+NicolasAvailable@users.noreply.github.com>
2024-02-07 20:55:17 -0400

    add: initial architecture

---
+++ src/app/auth/views/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'view-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
